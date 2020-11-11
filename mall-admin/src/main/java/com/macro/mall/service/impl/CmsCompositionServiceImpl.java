35414582e04deeed55dd4ccafb508c073af3214f
yongming <yongming.xia@live.com>
2020-11-12 01:21:22 +0800

    update compositionService

--- mall-admin/src/main/java/com/macro/mall/service/impl/CmsCompositionServiceImpl.java
+++ mall-admin/src/main/java/com/macro/mall/service/impl/CmsCompositionServiceImpl.java
@@ -2,15 +2,18 @@ package com.macro.mall.service.impl;
 
 import java.util.List;
 
+import com.github.pagehelper.PageHelper;
 import com.macro.mall.dto.CmsCompositionParam;
 import com.macro.mall.dto.CmsCompositionQueryParam;
 import com.macro.mall.mapper.CmsCompositionMapper;
 import com.macro.mall.model.CmsComposition;
+import com.macro.mall.model.CmsCompositionExample;
 import com.macro.mall.service.CmsCompositionService;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 @Service
 public class CmsCompositionServiceImpl implements CmsCompositionService {
@@ -25,32 +28,55 @@ public class CmsCompositionServiceImpl implements CmsCompositionService {
     }
 
     @Override
-    public List<CmsComposition> listCompositions(CmsCompositionQueryParam queryParam, Integer pageNum,
-            Integer pageSize) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+    public List<CmsComposition> listCompositions(final CmsCompositionQueryParam queryParam, final Integer pageNum,
+            final Integer pageSize) {
+                PageHelper.startPage(pageNum, pageSize);
+                final CmsCompositionExample compositionExample = new CmsCompositionExample();
+                final CmsCompositionExample.Criteria criteria = compositionExample.createCriteria();
+                if(!StringUtils.isEmpty(queryParam.getTitle()))
+                {
+                    criteria.andTitleLike("%"+queryParam.getTitle()+"%");
+                }
+                if(queryParam.getStatus()!=null)
+                {
+                    criteria.andStatusEqualTo(queryParam.getStatus());
+                }
+                if(!StringUtils.isEmpty(queryParam.getAuthor()))
+                {
+                    criteria.andTitleLike("%"+queryParam.getAuthor()+"%");
+                }
+                if(!StringUtils.isEmpty(queryParam.getReviewer()))
+                {
+                    criteria.andTitleLike("%"+queryParam.getReviewer()+"%");
+                }
+                return    compositionMapper.selectByExample(compositionExample);
+         
+            }
 
     @Override
-    public int createComposition(CmsCompositionParam compositcionParam) {
+    public int createComposition(final CmsCompositionParam compositcionParam) {
 
-        CmsComposition composition=new CmsComposition();
+        final CmsComposition composition=new CmsComposition();
         composition.setReadCount(0);
         BeanUtils.copyProperties(compositcionParam, composition);
         
-        int count= compositionMapper.insertSelective(composition);
+        final int count= compositionMapper.insertSelective(composition);
 
         return count;
     }
 
     @Override
-    public int update(Long id, CmsCompositionParam compositcionParam) {
-        // TODO Auto-generated method stub
-        return 0;
+    public int update(final Long id, final CmsCompositionParam compositcionParam) {
+        final CmsComposition composition=new CmsComposition();
+        composition.setReadCount(0);
+        BeanUtils.copyProperties(compositcionParam, composition);
+        final CmsCompositionExample example = new CmsCompositionExample();
+        example.createCriteria().andIdEqualTo(id);
+        return compositionMapper.updateByExampleSelective(composition, example);
     }
 
     @Override
-    public CmsComposition getComposition(Long id) {
+    public CmsComposition getComposition(final Long id) {
         // TODO Auto-generated method stub
         return null;
     }
